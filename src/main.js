a7f10d8dfb7aecb7a981ff96618428bc3c6be927
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-06-18 12:06:40 +0200

    En el archivo main.js se importan los ficheros para el correcto funcionamiento de bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 const app = createApp(App)
 
 app.use(router)
