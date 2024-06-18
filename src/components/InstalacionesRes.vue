cf9a3f9586744a392e61224b9c8396e4d93e663b
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-06-18 22:00:43 +0200

    Se crean los componentes InstalacionesRes.vue y NuestraHistoria.vue y se añaden a la vista AboutView.vue

---
+++ src/components/InstalacionesRes.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="p-4">
+    <h2 class="my-3">Instalaciones</h2>
+    <h3 class="my-3">Diferentes ambientes en nuestras instalaciones</h3>
+
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <p>
+          Contamos con diferentes ambientes para que puedas disfrutar de tu comida en el lugar que
+          más te guste.
+        </p>
+        <p>El ambiente divertido, festivo y de recreo de la barra.</p>
+        <p>
+          También puedes disfrutar del ambiente calido y tranquilo que te proporciona el comedor.
+        </p>
+        <img
+          class="w-100 my-2"
+          src="../assets/img/comedor.jpg"
+          alt="Comedor en vista nocturna"
+          title="Comedor en vista nocturna"
+        />
+      </div>
+      <div class="col-12 col-lg-6">
+        <img
+          class="w-100 my-2"
+          src="../assets/img/comedor_invierno.jpg"
+          alt="Mesa para grupo grande adornada con flores"
+          title="Mesa para grupo grande"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <p>
+          Y si prefieres disfrutar de la comida al aire libre, puedes hacerlo en nuestra terraza
+          para tomar el sol y el aire, o al fresco en las noches de verano.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <img
+          class="w-100 my-2"
+          src="../assets/img/mas_jardin.jpg"
+          alt="Vista diurna de la Terraza"
+          title="Vista diurna de la Terraza"
+        />
+      </div>
+      <div class="col-12 col-lg-6">
+        <img
+          class="w-100 my-2"
+          src="../assets/img/terraza.jpg"
+          alt="Terraza al anochecer"
+          title="Terraza al anochecer"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div className="container2 mx-auto">
+    <h1 class="px-4">Sobre Nosotros</h1>
+    <div>
+      <InstalacionesRes />
+      <NuestraHistoria />
+      <!-- {/* TODO: Agregar más secciones */}
+        {/* <div>Nuestro Equipo</div>
+        <div>Perfil de la Cocinera</div> */} -->
+    </div>
   </div>
 </template>
 
-<style></style>
+<script>
+import InstalacionesRes from '@/components/InstalacionesRes.vue'
+import NuestraHistoria from '@/components/NuestraHistoria.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    InstalacionesRes,
+    NuestraHistoria
+  }
+}
+</script>
+
+<style scoped></style>
