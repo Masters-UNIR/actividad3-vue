f8f8c349b6974608359f40605f4426e528f944f2
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-06-18 12:04:30 +0200

    Se actualiza el fichero de rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import EnvironmentView from '../views/EnvironmentView.vue'
+import ContactoView from '../views/ContactoView.vue'
+import ErrorView from '../views/ErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
@@ -15,7 +19,23 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      // component: () => import('../views/AboutView.vue')
+      component: AboutView
+    },
+    {
+      path: '/environment',
+      name: 'environment',
+      component: EnvironmentView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactoView
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: ErrorView
     }
   ]
 })
