90b937ca4b3cdaa601651145f109c981c4dec337
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-06-18 21:27:29 +0200

    Se ajusta padding en CarrousImages.vue. Se desinstala bootstrap-vue

--- src/components/CarrousImages.vue
+++ src/components/CarrousImages.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="carouselExampleCaptions"
-    class="carousel slide container2 mx-auto"
+    class="carousel slide container2 mx-auto p-3"
     data-bs-ride="carousel"
   >
     <div class="carousel-indicators">
--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
