29a53d806b58c111d73482498c80c09d8be2e14e
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-06-19 11:12:18 +0200

    Se instala la dependencia vue3-google-map y se crea el componente GoogleMap.vue que es importado por la vista ContactoView.vue. Se implementa la vista ContactoView.vue

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="gmap">
+    <GoogleMap :api-key="apiKey" style="width: 100%; height: 600px" :center="center" :zoom="16">
+      <!-- <Marker :options="{ position: center }" /> -->
+    </GoogleMap>
+  </div>
+</template>
+
+<script setup>
+import { GoogleMap } from 'vue3-google-map'
+
+const center = { lat: 42.1148, lng: 3.144766 }
+
+const apiKey = import.meta.env.VITE_VUE_GOOGLE_MAPS_API_KEY
+</script>
+
+<style scoped>
+.gmap {
+  flex: 0 0 auto;
+  width: 100%;
+  height: 600px;
+  max-width: 1160px;
+  margin: 2rem auto;
+  margin-bottom: 20px;
+}
+</style>
--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>This is de Contact Page</div>
+  <div class="container2 mx-auto text-center">
+    <h1>Contactanos</h1>
+    <div class="row">
+      <div class="col-12">
+        <!-- <img src="" alt="" /> -->
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2>Dirección</h2>
+        <p>Av. de Riells, 114B 17130 L&apos;Escala</p>
+      </div>
+      <div class="col-12">
+        <h2>Coordenadas GPS</h2>
+        <p>Latitud 42.114800 Longitud 3.144766</p>
+      </div>
+      <div class="col-12">
+        <h2>Teléfonos</h2>
+        <p>972 793 235 &nbsp;&nbsp;&nbsp; 607 951 936</p>
+      </div>
+      <div class="col-12">
+        <h2>Abrimos</h2>
+        <h3>Durante el año</h3>
+        <p>Mie - Dom 1pm - 17pm (comidas)</p>
+        <p>Vie - Sat 8pm - 1am (cenas)</p>
+        <h3>En Verano Julio - Agosto</h3>
+        <p>Mie - Dom 1pm - 17pm (comidas)</p>
+        <p>Mie - Dom 8pm - 2am (cenas)</p>
+      </div>
+    </div>
+    <div class="row">
+      <GoogleMap />
+    </div>
+  </div>
 </template>
 <script>
-export default {}
+import GoogleMap from '@/components/GoogleMap.vue'
+export default {
+  name: 'ContactoView',
+  components: {
+    GoogleMap
+  }
+}
 </script>
 <styles scoped>
-    
+
 </styles>
